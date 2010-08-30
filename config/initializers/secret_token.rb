1100da45af459df161dccf304866688d2a734625
Ryan Alyn Porter <rap@endymion.com>
2010-08-30 12:24:57 -0400

    Initial commit to jessie-is-cool.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JessieIsCool::Application.config.secret_token = 'f1ba714422afa2950737356923c6c9622ea190389a1e82760363b7e0b0e92ea45c5ac8396c58cd9c50fa5b796229ee1c060c8705ce30e79882f563224c29eacf'
